62ef3bffe375af8c493240371b498be5cb09c7da
swuff-star <63352264+swuff-star@users.noreply.github.com>
2024-04-16 21:34:40 -0400

    ummmm derp?
    -got thing wroking on my pc yippee
    -quickstep wip kinda lazy  heavily adjusted roll copy atm, ironing out feel need to do the actual parry mechanic entirely lol
    -updated some tokens :grin:

---
+++ HunkProject/SkillStates/Hunk/Step.cs
@@ -0,0 +1,115 @@
+﻿using EntityStates;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace HunkMod.SkillStates.Hunk
+{
+    public class Step : BaseHunkSkillState
+    {
+        protected Vector3 slipVector = Vector3.zero;
+        public float duration = 1.1f;
+        //private Vector3 cachedForward;
+        private float coeff = 24f;
+        private bool slowFlag = false;
+        private bool slowFlag2 = false;
+        private bool slowFlag3 = false;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            this.hunk.isRolling = true;
+            this.slipVector = ((base.inputBank.moveVector == Vector3.zero) ? base.characterDirection.forward : base.inputBank.moveVector).normalized;
+            //this.cachedForward = this.characterDirection.forward;
+
+            /*Animator anim = this.GetModelAnimator();
+
+            Vector3 rhs = base.characterDirection ? base.characterDirection.forward : this.slipVector;
+            Vector3 rhs2 = Vector3.Cross(Vector3.up, rhs);
+            float num = Vector3.Dot(this.slipVector, rhs);
+            float num2 = Vector3.Dot(this.slipVector, rhs2);
+            anim.SetFloat("dashF", num);
+            anim.SetFloat("dashR", num2);*/
+
+            base.PlayCrossfade("FullBody, Override", "DodgeFull", "Dodge.playbackRate", this.duration * 1.4f, 0.05f);
+            base.PlayAnimation("Gesture, Override", "BufferEmpty");
+
+            Util.PlaySound("sfx_driver_dash", this.gameObject);
+
+            EntityStateMachine.FindByCustomName(this.gameObject, "Aim").SetNextStateToMain();
+            this.skillLocator.secondary.stock = 0;
+            this.skillLocator.secondary.rechargeStopwatch = -0.3f;
+
+            this.CreateDashEffect();
+        }
+
+        public virtual void CreateDashEffect()
+        {
+            /*EffectData effectData = new EffectData();
+            effectData.rotation = Util.QuaternionSafeLookRotation(this.slipVector);
+            effectData.origin = base.characterBody.corePosition;
+
+            EffectManager.SpawnEffect(Modules.Assets.dashFX, effectData, false);*/
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            this.characterBody.aimTimer = -1f;
+            this.hunk.reloadTimer = 1f;
+            this.inputBank.moveVector = Vector3.zero;
+            base.characterMotor.velocity = Vector3.zero;
+            base.characterMotor.rootMotion = this.slipVector * (this.coeff * Time.fixedDeltaTime) * Mathf.Cos(base.fixedAge / this.duration * 1.57079637f);
+
+            if (base.isAuthority)
+            {
+                if (base.characterDirection)
+                {
+                    //base.characterDirection.forward = this.cachedForward;
+                    base.characterDirection.forward = this.slipVector;
+                }
+            }
+
+            if (!this.slowFlag && base.fixedAge >= (0.05f * this.duration))
+            {
+                this.slowFlag = true;
+                this.coeff = 7f;
+            }
+
+            if (!this.slowFlag2 && base.fixedAge >= (0.25f * this.duration))
+            {
+                this.slowFlag2 = true;
+                this.coeff = 14f;
+            }
+            if (!this.slowFlag3 && base.fixedAge >= (0.85f * this.duration))
+            {
+                this.slowFlag3 = true;
+                this.coeff = 4f;
+            }
+
+            if (base.isAuthority && base.fixedAge >= this.duration)
+            {
+                this.outer.SetNextStateToMain();
+            }
+        }
+
+        public virtual void DampenVelocity()
+        {
+            base.characterMotor.velocity *= 0.75f;
+        }
+
+        public override void OnExit()
+        {
+            this.DampenVelocity();
+            this.hunk.isRolling = false;
+
+            base.OnExit();
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            if (this.slowFlag) return InterruptPriority.Any;
+            return InterruptPriority.Frozen;
+        }
+    }
+}
--- HunkProject/obj/Debug/netstandard2.0/HunkMod.AssemblyInfo.cs
+++ HunkProject/obj/Debug/netstandard2.0/HunkMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HunkMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1332635b5d62e411dd53bc87a70d69bae5662c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("HunkMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HunkMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
